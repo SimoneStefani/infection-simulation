d5e8e4b427394f8fceb4b12e30ddd76576fed0f4
cedric <cedric.seger.1@gmail.com>
2017-09-27 13:37:38 +0200

    Add arg parsing functionality for terminal input

--- backend/app/simulation.py
+++ backend/app/simulation.py
@@ -1,40 +1,8 @@
-from .gridworld import GridWorld
+from gridworld import GridWorld
 import argparse
 import matplotlib.pyplot as plt
 
-# import matplotlib.animation as animation
-
-# gridworld = GridWorld(world_size=15, infected_locations=[(0,0)])
-
-# print(gridworld.get_world_map_values())
-# ticks = 0
-# while(gridworld.count([1,2])):
-# 	gridworld.tick()
-# 	ticks += 1
-# 	print('tick..')
-# print('FINISHED in {0} ticks!'.format(ticks))
-# print(gridworld.get_world_map_values())
-
-# plt.imshow(gridworld.get_world_map_values())
-# plt.colorbar()
-# plt.show()
-
 gridworld = None
-
-def go(args):
-	gridworld = GridWorld(world_size=args.N, infected_locations=[(0,0)])
-	print(gridworld.get_world_map_values())
-	ticks = 0
-	while(gridworld.count([1,2])):
-		gridworld.tick()
-		ticks += 1
-		print('tick..')
-	print('FINISHED in {0} ticks!'.format(ticks))
-	print(gridworld.get_world_map_values())
-
-	plt.imshow(gridworld.get_world_map_values())
-	plt.colorbar()
-	plt.show()
 
 def genWorld():
 	global gridworld 
@@ -46,15 +14,58 @@ def tickSim():
 	return gridworld.get_world_map_values()
 
 
-def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--N", help="Size of population", type=int, default=2, choices=range(0,20))
-	args = parser.parse_args()
+def main(args):
+	starting_locations = list(zip(args.locations[0::2], args.locations[1::2]))
 	
-	go(args)
+	gridworld = GridWorld(world_size=args.n, infected_locations=starting_locations)
+	# print(gridworld.get_world_map_values())
+	ticks = 0
+	while(gridworld.count([1,2])):
+		gridworld.tick()
+		ticks += 1
+		print('tick..')
+	print('FINISHED in {0} ticks!'.format(ticks))
+	# print(gridworld.get_world_map_values())
+
+	plt.imshow(gridworld.get_world_map_values())
+	plt.colorbar()
+	plt.show()
+
+
+def restricted_float(x):
+	x = float(x)
+	if x < 0.0 or x > 1.0:
+		msg = '{} not in range [0.0, 1.0]'.format(x)
+		raise argparse.ArgumentTypeError(msg)
+	return x
+
+def check_day_range(day_range):
+	if day_range[0] > day_range[1]:
+		msg = 'argument --sick_days_min_max: Specified range: {} does not follow format [min, max]'.format(day_range)
+		raise argparse.ArgumentTypeError(msg)
+
+def check_locations(locations, world_size):
+	if len(locations) % 2 != 0:
+		msg = 'argument --locations: {} is not an even number of coordinates'.format(locations)
+		raise argparse.ArgumentTypeError(msg)
+	
+	for coordinate in locations:
+		if coordinate not in range(0,world_size+1):
+			msg = 'argument --locations: coordinate {0} must be in range [0,{1}]'.format(coordinate, world_size)
+			raise argparse.ArgumentTypeError(msg)
 
 
 if __name__ == '__main__':
-	print('Running script...\n')
-	main()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--n", help="Size of population", type=int, default=2, choices=range(2,20))
+	parser.add_argument('--locations', nargs='+', type=int, required=True)
+	parser.add_argument('--chance_of_infection', type=restricted_float, default=0.50)
+	parser.add_argument('--chance_of_death', type=restricted_float, default=0.50)
+	parser.add_argument('--sick_days_min_max', nargs='+', type=int, default=[1, 6])
+	args = parser.parse_args()
+
+	check_day_range(args.sick_days_min_max)
+	check_locations(args.locations, args.n)
+
+	main(args)
 
