875334b94d9a7d481977ec382addbf2acb044215
Simone Stefani <s.stefani95@gmail.com>
2017-09-27 22:05:37 +0200

    Format and clean code

--- backend/app/simulation.py
+++ backend/app/simulation.py
@@ -1,36 +1,51 @@
-from gridworld import GridWorld
-import util
+from .gridworld import GridWorld
+from .util import parse_input
 import matplotlib.pyplot as plt
 
-gridworld = None
+# Global instance of GridWorld. It is used by the client in order
+# to tick every time the same instance of the GridWorld.
+grid_world = None
 
-def genWorld():
-	global gridworld 
-	gridworld = GridWorld(world_size=40, infected_locations=[(5,20), (35,4)])
 
-def tickSim():
-	global gridworld
-	gridworld.tick()
-	return gridworld.get_world_map_values()
+# Generate a global instance of GridWorld. This function is mainly
+# used by the JavaScript client when the user press play.
+def generate_world():
+    global grid_world
+    grid_world = GridWorld({
+        "world_size": 40,
+        "infected_locations": [(5, 20), (35, 4)],
+        "sick_days_min_max": [1, 6],
+        "chance_of_death": 0.5,
+        "chance_of_infection": 0.5
+    })
+
+
+# Advance the simulation on the global instance of GridWorld of
+# one step. Return the new configuration of GridWorld.
+def tick_simulation():
+    global grid_world
+    grid_world.tick()
+    return grid_world.get_world_map_values()
+
 
 def main(args):
-	gridworld = GridWorld(args)
-	ticks = 0
+    grid_world = GridWorld(args)
+    ticks = 0
 
-	while(True):
-		sick_count = gridworld.tick()
-		ticks += 1
-		print('tick..')
-		if (sick_count <= 0):
-			break
+    while True:
+        sick_count = grid_world.tick()
+        ticks += 1
+        print('tick..')
+        if sick_count <= 0:
+            break
 
-	print('FINISHED in {0} ticks!'.format(ticks))
+    print('FINISHED in {0} ticks!'.format(ticks))
 
-	plt.imshow(gridworld.get_world_map_values())
-	plt.colorbar()
-	plt.show()
+    plt.imshow(grid_world.get_world_map_values())
+    plt.colorbar()
+    plt.show()
+
 
 if __name__ == '__main__':
-	args = util.parse_input()
-	main(args)
-
+    args = parse_input()
+    main(args)
--- backend/app/util.py
+++ backend/app/util.py
@@ -1,40 +1,58 @@
 import argparse
 
+
 def parse_input():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--world_size", help="Size of population", type=int, default=2, choices=range(2,21))
-	parser.add_argument('--infected_locations', nargs='+', type=int, required=True,)
-	parser.add_argument('--chance_of_infection', type=restricted_float, default=0.50)
-	parser.add_argument('--chance_of_death', type=restricted_float, default=0.50)
-	parser.add_argument('--sick_days_min_max', nargs='+', type=int, default=[1, 6])
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--world_size", help="Size of population", type=int,
+                        default=2, choices=range(2, 21))
+    parser.add_argument('--infected_locations', nargs='+', type=int,
+                        required=True, )
+    parser.add_argument('--chance_of_infection', type=restricted_float,
+                        default=0.50)
+    parser.add_argument('--chance_of_death', type=restricted_float,
+                        default=0.50)
+    parser.add_argument('--sick_days_min_max', nargs='+', type=int,
+                        default=[1, 6])
+    args = parser.parse_args()
+    print(args)
 
-	check_day_range(args.sick_days_min_max)
-	check_locations(args.infected_locations, args.world_size)
+    check_day_range(args.sick_days_min_max)
+    check_locations(args.infected_locations, args.world_size)
 
-	# Zip input in form of 1 2 3 4 to coordinate tuples of form [(1,2), (3,4)]
-	args.infected_locations = list(zip(args.infected_locations[0::2], args.infected_locations[1::2]))
+    # Zip input in form of 1 2 3 4 to coordinate tuples of form [(1,2), (3,4)]
+    args.infected_locations = list(
+        zip(args.infected_locations[0::2], args.infected_locations[1::2]))
 
-	return args
+    return args
 
+
+# Ensure that the users specifies valid probability values.
 def restricted_float(x):
-	x = float(x)
-	if x < 0.0 or x > 1.0:
-		msg = '{} not in range [0.0, 1.0]'.format(x)
-		raise argparse.ArgumentTypeError(msg)
-	return x
+    x = float(x)
+    if x < 0.0 or x > 1.0:
+        msg = '{} not in range [0.0, 1.0]'.format(x)
+        raise argparse.ArgumentTypeError(msg)
+    return x
 
+
+# Ensure that the days specified by the users follow a logical order.
 def check_day_range(day_range):
-	if day_range[0] > day_range[1]:
-		msg = 'argument --sick_days_min_max: Specified range: {} does not follow format [min, max]'.format(day_range)
-		raise argparse.ArgumentTypeError(msg)
+    if day_range[0] > day_range[1]:
+        msg = 'argument --sick_days_min_max: Specified range: {} does not follow format [min, max]'.format(
+            day_range)
+        raise argparse.ArgumentTypeError(msg)
 
+
+# Ensure that the locations input of the user are inside the
+# boundaries of the GridWorld.
 def check_locations(locations, world_size):
-	if len(locations) % 2 != 0:
-		msg = 'argument --locations: {} is not an even number of coordinates'.format(locations)
-		raise argparse.ArgumentTypeError(msg)
-	
-	for coordinate in locations:
-		if coordinate not in range(0,world_size+1):
-			msg = 'argument --locations: coordinate {0} must be in range [0,{1}]'.format(coordinate, world_size)
-			raise argparse.ArgumentTypeError(msg)
+    if len(locations) % 2 != 0:
+        msg = 'argument --locations: {} is not an even number of coordinates'.format(
+            locations)
+        raise argparse.ArgumentTypeError(msg)
+
+    for coordinate in locations:
+        if coordinate not in range(0, world_size + 1):
+            msg = 'argument --locations: coordinate {0} must be in range [0,{1}]'.format(
+                coordinate, world_size)
+            raise argparse.ArgumentTypeError(msg)
